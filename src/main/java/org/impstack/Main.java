8ad3484f14b44db827f091388eb1095657b4d86e
remy <remy@impstack.org>
2018-02-23 15:42:36 +0100

    add assets folder, configure logging, add a main class that displays a blue cube

--- src/main/java/org/impstack/Main.java
+++ src/main/java/org/impstack/Main.java
@@ -1,8 +1,31 @@
 package org.impstack;
 
-public class Main {
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
+import org.impstack.jme.JmeLauncher;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main extends JmeLauncher {
+
+    public static void main( String... args ) {
+        Main main = new Main();
+        main.start();
     }
+
+    @Override
+    public void init() {
+        Box box = new Box(1, 1, 1);
+        Geometry geom = new Geometry("box", box);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+
+        Texture texture = assetManager.loadTexture("Textures/Monkey.png");
+        mat.setTexture("ColorMap", texture);
+
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);
+    }
+
 }
