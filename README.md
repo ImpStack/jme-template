f9ecd538e607873dadd419472f4b789609d326f1
remy <remy@impstack.org>
2018-09-18 11:36:24 +0200

    OpenGL3 support

--- README.md
+++ README.md
@@ -20,3 +20,24 @@ To get an overview of all gradle tasks use:
 ```
 gradlew tasks
 ```
+
+## Running on OpenGL3.3 and higher
+When you want to use OpenGL3.3 or higher, you need to include the `jme3-lwjgl3` library in your project.
+Adapt this line in the `build.gradle` file:
+
+> compile "org.jmonkeyengine:jme3-lwjgl:${jmeVersion}"
+
+to
+
+> compile "org.jmonkeyengine:jme3-lwjgl3:${jmeVersion}"
+
+Make sure that other libraries aren't including older versions of lwjgl! This could lead to sealing exceptions thrown by java.lang.Security. 
+
+### Known issues
+The following are known issues and a way solve them.
+
+**Error:**
+java.lang.IllegalStateException: Please run the JVM with -XstartOnFirstThread and make sure a window toolkit other than GLFW (e.g. AWT or JavaFX) is not initialized.
+
+**Solution:** 
+Add `org.lwjgl.system.Configuration.GLFW_CHECK_THREAD0.set(false);` before calling the `application.start()` method.
--- src/main/java/org/impstack/Main.java
+++ src/main/java/org/impstack/Main.java
@@ -1,11 +1,10 @@
 package org.impstack;
 
-import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
 import org.impstack.jme.JmeLauncher;
+import org.impstack.jme.scene.GeometryUtils;
 
 public class Main extends JmeLauncher {
 
@@ -16,16 +15,8 @@ public class Main extends JmeLauncher {
 
     @Override
     public void init() {
-        Box box = new Box(1, 1, 1);
-        Geometry geom = new Geometry("box", box);
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-
-        Texture texture = assetManager.loadTexture("Textures/Monkey.png");
-        mat.setTexture("ColorMap", texture);
-
-        geom.setMaterial(mat);
-        rootNode.attachChild(geom);
+        Geometry geometry = new GeometryUtils(this).createGeometry(new Box(1, 1, 1), ColorRGBA.Blue, "Textures/Monkey.png");
+        rootNode.attachChild(geometry);
     }
 
 }
